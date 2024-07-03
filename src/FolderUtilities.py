0470cc6d750bae886da4a4da396530fcdaf9d395
Chiara <chr.mallegni@gmail.com>
2024-07-03 22:43:40 +0200

    commit first FolderUtilities

---
+++ src/FolderUtilities.py
@@ -0,0 +1,22 @@
+import os
+
+class FolderUtilities:
+    def __init__(self, parent_dir, folder, mode):
+        self.parent_dir=parent_dir
+        self.folder=folder
+        self.mode=mode
+
+# create folder if not exist
+def create_folder(parent_dir, folder, mode):
+    directory = os.path.join(parent_dir,folder)
+    if os.path.exists(directory):
+        print("Directory '%s' exists" % directory)
+    if not os.path.exists(directory):
+        os.mkdir(directory, mode)
+        print("Directory '%s' created" % directory)
+    return directory
+
+
+
+
+
